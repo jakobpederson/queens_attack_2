67911c1afe5db0610986d00b73fc5df238abe1bf
jakobpederson <jakob.pederson+github@gmail.com>
2017-08-06 10:28:47 -0500

    wip

--- queens_attack_2.py
+++ queens_attack_2.py
@@ -1,3 +1,6 @@
+from collections import namedtuple
+
+Coordinates = namedtuple("Coordinates", ["y", "x"])
 
 class QueensAttack():
 
@@ -8,4 +11,10 @@ class QueensAttack():
     def get_left(self, queen, length):
         x = [x for x in range(length)]
         return len(x[:queen])
+
+    def solve_east_west(self, queen, length):
+        coord = Coordinates(y=queen[0], x=queen[1])
+        sum = self.get_right(coord.x, length) + self.get_right(coord.y, length) +  self.get_left(coord.x - 1, length) +  self.get_left(coord.y - 1, length)
+        return sum
+
 
--- test_queens_attack_2.py
+++ test_queens_attack_2.py
@@ -17,8 +17,16 @@ class QueensAttackTest(unittest.TestCase):
         self.assertEqual(1, self.sut.get_left(1, 4))
         self.assertEqual(2, self.sut.get_left(2, 4))
 
-    def test_x(self):
+    def test_right_left(self):
         self.assertEqual(3, self.sut.get_left(4 - 1, 4))
         self.assertEqual(0, self.sut.get_right(4, 4))
         self.assertEqual(2, self.sut.get_left(3 - 1, 5))
         self.assertEqual(2, self.sut.get_right(3, 5))
+
+    def test_up_down(self):
+        self.assertEqual(6, self.sut.solve_east_west((4, 4), 4))
+        self.assertEqual(8, self.sut.solve_east_west((4, 3), 5))
+
+    def test_diagonals(self):
+        self.assertEqual(6, self.sut.solve_east_west((4, 4), 4))
+        self.assertEqual(8, self.sut.solve_east_west((4, 3), 5))
